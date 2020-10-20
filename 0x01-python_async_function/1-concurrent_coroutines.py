741ca8f60846b93accf45f8840c80dc9a12c3eb7
yawzyag <yesid.dev93@gmail.com>
2020-10-20 18:07:56 -0500

    [Feature] multiple corutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,10 +11,19 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
+async def coro(seq) -> list:
+    """'IO' wait time is proportional to the max element."""
+    await asyncio.sleep(max(seq))
+    return list(reversed(seq))
+
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """date a number of times to execute (n) and max_delay to call"""
-    list = []
-    for i in range(0, n):
-        value = await wait_random(max_delay)
-        list.append(value)
-    return list
+    listValues = []
+    listResults = []
+    for i in range(n):
+        listValues.append(asyncio.create_task(wait_random(max_delay)))
+
+    for task in asyncio.as_completed(listValues):
+        listResults.append(await task)
+    return listResults
