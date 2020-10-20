0de08f5bc3e3b517764b0fa2943b8520873b3b44
yawzyag <yesid.dev93@gmail.com>
2020-10-20 17:41:17 -0500

    [Feature] concurency

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,7 +11,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[wait_random]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """date a number of times to execute (n) and max_delay to call"""
     list = []
     for i in range(0, n):
