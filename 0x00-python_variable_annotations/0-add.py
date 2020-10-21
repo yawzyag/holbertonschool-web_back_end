28b1f7668f0ba0c94a1cea9611a78409de2d6d26
yawzyag <yesid.dev93@gmail.com>
2020-10-21 11:39:17 -0500

    [Feature] add two numbers

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""async await
+tip for basic use of async await
+"""
+
+
+def add(a: float, b: float) -> float:
+    """take a delay and await randomly for the result"""
+    return a+b
