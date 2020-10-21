a32c3912075c180ec585040a8ec43ab8d0f43af0
yawzyag <yesid.dev93@gmail.com>
2020-10-21 11:49:21 -0500

    [Feature] floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""floor operation
+get the decimal floor part
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """takes n and returns his decimal floor part"""
+    return math.floor(n)
