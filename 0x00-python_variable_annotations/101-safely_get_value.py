c8dc96e38e17386751b97f0b62a7d5c24327d380
yawzyag <yesid.dev93@gmail.com>
2020-10-21 13:17:44 -0500

    [Feature] comments

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -11,6 +11,16 @@ T = TypeVar('T', bound=Callable[...,  Optional[int]])
 
 def safely_get_value(dct: Mapping, key: Any,
                      default: Union[T, None] = None) -> Union[Any, T]:
+    """get the value of a dict
+
+    Args:
+        dct (Mapping): [dict to check]
+        key (Any): [key to get]
+        default (Union[T, None], optional): [default]. Defaults to None.
+
+    Returns:
+        Union[Any, T]: [the value found]
+    """
     if key in dct:
         return dct[key]
     else:
