119f97a71f564df3a009bae240e3fe602e582411
yawzyag <yesid.dev93@gmail.com>
2020-10-21 12:02:52 -0500

    [Feature] sum list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""basic operation
+sum list values
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """takes input list and returns his sum"""
+    return sum(input_list)
