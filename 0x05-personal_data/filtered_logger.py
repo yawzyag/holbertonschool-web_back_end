d77fa5c643176acb10269a4345922ca5fc467056
yawzyag <yesid.dev93@gmail.com>
2020-11-02 13:36:19 -0500

    [Feature] logger

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,6 +4,37 @@ filter specific data
 """
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """[constructor]
+
+        Args:
+            fields (List[str]): [fileds to check]
+        """
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """[format log]
+
+        Args:
+            record (logging.LogRecord): [record to format]
+
+        Returns:
+            str: [format logger]
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -19,6 +50,7 @@ def filter_datum(fields: List[str], redaction: str,
     Returns:
         str: [final filter message]
     """
+    print(message)
     for val in fields:
         match = re.search(rf'{val}=([^{separator}#]*)', message)
         if match and val:
