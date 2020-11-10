82724000ea61056e8a1003ce7885bdc364b5f8b5
yawzyag <yesid.dev93@gmail.com>
2020-11-10 15:53:17 -0500

    [Feature] check for session cookie

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -31,9 +31,12 @@ def before_request_func():
     """
     if (auth is not None):
         excluded = ['/api/v1/status/',
-                    '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                    '/api/v1/unauthorized/',
+                    '/api/v1/forbidden/',
+                    '/api/v1/auth_session/login/']
         if (auth.require_auth(request.path, excluded)):
-            if (auth.authorization_header(request) is None):
+            if (auth.authorization_header(request) is None and
+                    auth.session_cookie(request) is None):
                 abort(401)
             if (auth.current_user(request) is None):
                 abort(403)
