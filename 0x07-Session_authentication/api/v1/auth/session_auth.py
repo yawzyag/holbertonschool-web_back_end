40e5175b0b1cd73fd83d42239e1b076cce028712
yawzyag <yesid.dev93@gmail.com>
2020-11-10 15:33:22 -0500

    [Feature] session id

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@ from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 import base64
+import uuid
 from models.user import User
 
 
@@ -14,3 +15,19 @@ class SessionAuth(Auth):
     Args:
         Auth ([class]): [class auth]
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """[create sesion for user]
+
+        Args:
+            user_id (str, optional): [id]. Defaults to None.
+
+        Returns:
+            str: [id]
+        """
+        if user_id and isinstance(user_id, str):
+            id = str(uuid.uuid4())
+            self.user_id_by_session_id[id] = user_id
+            return id
+        return None
