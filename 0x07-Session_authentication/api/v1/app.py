2baae679afa3061432643f41dd6521ca6fef8042
yawzyag <yesid.dev93@gmail.com>
2020-11-11 18:36:18 -0500

    [Feature] error

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -11,7 +11,6 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
-from api.v1.auth.session_db_auth import SessionDBAuth
 
 
 app = Flask(__name__)
@@ -25,10 +24,6 @@ if (os.getenv("AUTH_TYPE")):
         auth = Auth()
     elif os.getenv("AUTH_TYPE") == "session_auth":
         auth = SessionAuth()
-    elif os.getenv("AUTH_TYPE") == "session_exp_auth":
-        auth = SessionExpAuth()
-    elif os.getenv("AUTH_TYPE") == "session_db_auth":
-        auth = SessionDBAuth()
 
 
 @app.before_request
