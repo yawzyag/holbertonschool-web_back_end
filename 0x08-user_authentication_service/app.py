64163ceb5f4ecfaa446c5768c58bd3a75871ed30
yawzyag <yesid.dev93@gmail.com>
2020-11-18 11:38:38 -0500

    [Feature] create user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,13 +2,47 @@
 """
 Route module for the API
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route('/')
 def hello_world():
+    """[hello world flask]
+
+    Returns:
+        [type]: [json message]
+    """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def create_user() -> str:
+    """[create user]
+
+    Returns:
+        str: [description]
+    """
+    rj = None
+    error_msg = None
+    try:
+        rj = request.form
+    except Exception as e:
+        rj = None
+
+    if error_msg is None and rj:
+        try:
+            email = rj.get("email")
+            password = rj.get("password")
+            AUTH.register_user(email, password)
+            return jsonify({"email": email,
+                            "message": "user created"}), 200
+        except Exception as e:
+            error_msg = {"message": "email already registered"}
+    return jsonify(error_msg), 400
 
 
 if __name__ == "__main__":
