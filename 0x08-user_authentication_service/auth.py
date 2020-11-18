3245dcdeb4ac998a9326640cc13173c651c93300
yawzyag <yesid.dev93@gmail.com>
2020-11-18 13:35:59 -0500

    [Feature] session id to None

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -106,3 +106,16 @@ class Auth:
             except Exception:
                 return None
         return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """[destroy session]
+
+        Args:
+            user_id (int): [id of user]
+        """
+        if (user_id):
+            try:
+                self._db.update_user(user_id, session_id=None)
+            except Exception:
+                return None
+        return None
