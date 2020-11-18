164a0856a1f5c676fb0822d769ccead4f8ed553d
yawzyag <yesid.dev93@gmail.com>
2020-11-18 14:08:51 -0500

    [Fix] ?

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -81,7 +81,7 @@ def logout():
     Returns:
         str: [state]
     """
-    session_id = request.cookies.get("session_id")
+    session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
     if (user):
         AUTH.destroy_session(user.id)
