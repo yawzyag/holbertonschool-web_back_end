1d7975caa18fd4404307dfb795fdd0eff92c9cb4
yawzyag <yesid.dev93@gmail.com>
2020-11-29 10:24:21 -0500

    [Feature] basic jinja2

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""[basic flask app]
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    """[basic template]
+
+    Returns:
+        [type]: [template]
+    """
+    title = "Welcome to Holberton"
+    hello = "Hello world"
+    return render_template('0-index.html',
+                           title=title, hello=hello)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="6090")
---
+++ 0x0A-i18n/README.md
@@ -0,0 +1,3 @@
+# i18n
+
+## Readme file
