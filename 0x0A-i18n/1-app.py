cd2303344c78e6b8f8bf5dc20e33a498982afd07
yawzyag <yesid.dev93@gmail.com>
2020-11-29 10:40:50 -0500

    [Feature] babel config

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""[basic flask app]
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+app = Flask(__name__)
+
+
+class Config():
+    """[config for babel]
+
+    Returns:
+        [type]: [config]
+    """
+    LANGUAGES = ["en", "fr"]
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def hello_world():
+    """[basic template]
+
+    Returns:
+        [type]: [template]
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
