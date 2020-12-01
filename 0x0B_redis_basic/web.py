3d92f3ad511580a16f79dd1ca364548df4d5100d
yawzyag <yesid.dev93@gmail.com>
2020-12-01 16:50:18 -0500

    [Feature] no expire count

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -31,12 +31,14 @@ def web_count(method: Callable) -> Callable:
         key = "count:" + args[0]
         # print('Calling decorated function')
         r.incr(key)
-        r.expire(key, timedelta(seconds=10))
+        data = r.get(args[0])
+        if (data is not None):
+            return data.decode("utf8")
 
         method_return = method(args[0])
         r.set(args[0], method_return)
         r.expire(args[0], timedelta(seconds=10))
-        return method(*args, **kwds)
+        return method_return
     return wrapper
 
 
