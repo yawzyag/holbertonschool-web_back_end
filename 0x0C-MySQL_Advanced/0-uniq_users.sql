346a9fce7970ab89c753694b744309612501169f
yawzyag <yesid.dev93@gmail.com>
2020-12-03 17:25:35 -0500

    [Feature] create table with unique value

---
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- users with unique email
+-- comment for table
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
