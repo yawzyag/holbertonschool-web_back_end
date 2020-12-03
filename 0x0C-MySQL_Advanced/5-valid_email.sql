13cb854af4e4f9170b0871f1d690642437c4be9d
yawzyag <yesid.dev93@gmail.com>
2020-12-03 18:47:33 -0500

    [Feature] reset

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- store values
+-- email trigger
+DELIMITER $$
+
+CREATE TRIGGER new_email
+BEFORE UPDATE
+ON users FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END$$
+
+DELIMITER ;
