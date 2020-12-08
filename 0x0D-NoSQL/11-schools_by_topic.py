5bac186a0836f664834f4c45f83ec27404a5c98a
yawzyag <yesid.dev93@gmail.com>
2020-12-08 16:22:35 -0500

    [Feature] find in array

---
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""[search in db]
+"""
+import re
+
+
+def schools_by_topic(mongo_collection, topic):
+    """[search by topic]
+
+    Args:
+        mongo_collection ([type]): [collection]
+        topic ([type]): [topic to search]
+    """
+    return mongo_collection.find({"topics": {"$all": [topic]}})
