211b0e2952f7e0cf9ab7102d40bc69454eaf6c56
yawzyag <yesid.dev93@gmail.com>
2020-12-10 18:41:08 -0500

    [Feature] arrow

---
+++ 0x0E-ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+export default function GetNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
