b5e4d92837de5e6fe8ca25b56554d86fbc2cb280
yawzyag <yesid.dev93@gmail.com>
2020-12-14 21:00:36 +0000

    [Feature] reject

---
+++ 0x0F-ES6_promise/1-promise.js
@@ -0,0 +1,8 @@
+const getFullResponseFromAPI = (success) => {
+  if (success) {
+    return Promise.resolve({ status: 200, body: 'Success' });
+  }
+  return Promise.reject(new Error('The fake API is not working currently'));
+};
+
+export default getFullResponseFromAPI;
