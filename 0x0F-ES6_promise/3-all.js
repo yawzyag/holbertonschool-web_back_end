7a1b13f975f033a1f95cb38f6144007d652242b7
yawzyag <yesid.dev93@gmail.com>
2020-12-16 01:05:02 +0000

    [Fix] spaces

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { createUser, uploadPhoto } from './utils';
 function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((data) => {
-      console.log(data[0].body, data[1].firstName, data[1].lastName);
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
