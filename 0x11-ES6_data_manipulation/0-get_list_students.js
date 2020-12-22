2511de52ee6abe0bb6d5636e84418d3b645bb5cb
yawzyag <yesid.dev93@gmail.com>
2020-12-22 17:58:55 +0000

    [Feature] list of students

---
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,18 @@
+const getListStudents = () => [
+  {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  },
+  {
+    id: 3,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  },
+];
+export default getListStudents;
