0c235315b6c67e4cf26977c80f51e995da762fb1
yawzyag <yesid.dev93@gmail.com>
2020-12-23 18:18:08 +0000

    [Feature] count

--- 0x11-ES6_data_manipulation/100-main.js
+++ 0x11-ES6_data_manipulation/100-main.js
@@ -2,7 +2,8 @@ import { queryAPI, weakMap } from './100-weak';
 
 const endpoint = { protocol: 'http', name: 'getUsers' };
 weakMap.get(endpoint);
-
+queryAPI({});
+queryAPI(endpoint);
 queryAPI(endpoint);
 console.log(weakMap.get(endpoint));
 
--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -1,8 +1,8 @@
 const weakMap = new WeakMap();
 const queryAPI = (endpoint) => {
-  let count = weakMap.get(endpoint) || 0;
+  const count = weakMap.get(endpoint) || 0;
   if (count >= 5) throw new Error('Endpoint load is high');
-  weakMap.set(endpoint, (count += 1));
+  weakMap.set(endpoint, count + 1);
 };
 
 export { weakMap, queryAPI };
